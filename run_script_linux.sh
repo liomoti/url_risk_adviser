9277ab68d6c60d71615e34970b39dedf6af8ead6
Moti Shaul <moti_shaul@bmc.com>
2024-04-09 22:41:45 +0300

    Update run_script_linux.sh

--- run_script_linux.sh
+++ run_script_linux.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Check if pip3 is installed
+if command -v pip3 &>/dev/null; then
+    # Install required packages
+    pip3 install -r requirements.txt
+    # Execute Python script
+    python3 "url_risk_adviser.py"
+else
+    echo "Error: pip3 not detected!"
+    echo "Please make sure you have pip3 and Python installed and try again."
+fi
 
-# Install required packages
-pip3 install -r requirements.txt
-
-# Execute Python script
-python3 "url_risk_adviser.py"
-
+# Prompt the user to press Enter before exiting
 read -p "Press Enter to continue..."
